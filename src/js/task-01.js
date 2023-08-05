8482f77147bdae60fa4c67ba4426c84750d8ca09
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-08-05 15:26:26 +0300

    видалено папку

--- src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const itemEl = document.querySelector("#categories");
-console.log(`Number of categories: ${itemEl.children.length}`);
-
-[...itemEl.children].forEach((number) => {
-  console.log(`Category: ${number.firstElementChild.textContent}`);
-  console.log(`Elements: ${number.lastElementChild.children.length}`);
-});
--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const inputElement = document.querySelector("#font-size-control");
-const spanElement = document.querySelector("#text");
-
-inputElement.addEventListener("input", onElementFontSize);
-function onElementFontSize(evt) {
-  let newSpanElement = evt.currentTarget.value;
-  console.log(newSpanElement);
-  spanElement.style.fontSize = `${newSpanElement}px`;
-}
